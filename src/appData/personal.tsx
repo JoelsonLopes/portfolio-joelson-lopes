02b1e56f748377c916c11b4f21f0c73ea6a64031
JoelsonLopes <joelsonlopes85@gmail.com>
2025-11-21 15:39:35 -0300

    feat(footer): add WhatsApp social link

--- src/appData/personal.tsx
+++ src/appData/personal.tsx
@@ -1,6 +1,10 @@
-import { GithubIcon, LinkedIn } from '@/utils/icons'
+import { GithubIcon, LinkedIn, WhatsAppIcon } from '@/utils/icons'
 
 export const socials = [
   { href: 'https://github.com/JoelsonLopes', icon: <GithubIcon /> },
   { href: 'https://www.linkedin.com/in/joelsonlopes/', icon: <LinkedIn /> },
+  {
+    href: 'https://wa.me/5551994325454',
+    icon: <WhatsAppIcon />,
+  },
 ]
